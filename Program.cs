ac25b0038a33504cd181338a4e45df520604fcb1
Saba Arefaine <arefainesaba@gmail.com>
2021-03-31 13:03:02 -0700

    inital commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your name?");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Hello"+ name + "!")
+        }
+    }
+}
